f197f4ec9020288ffa8b58a6c6358211c49efb69
Siddharth Menon <sm2947@rit.edu>
2023-02-03 19:53:54 -0500

    FurnaceFly Touchup
    The projectiles shot by the furnaceFly now correctly orient according to the direction they are fired in. Also added a couple markers that can be used to adjust the range of motion of the furnaceFly which should make it easier while setting up the level.

--- Time Zones/Assets/Scripts/FurnaceFly.cs
+++ Time Zones/Assets/Scripts/FurnaceFly.cs
@@ -4,45 +4,61 @@ using UnityEngine;
 
 public class FurnaceFly : MonoBehaviour
 {
-    public float yMax;
-    public float yMin;
-    public float speed;
-    public float attackCooldown;
-    public GameObject target;
-    public GameObject projectile;
-    public float projectileSpeed;
-    public float aggroRange;
+    
+    public float speed; //Speed of vertical motion of the furnaceFly
+    public float attackCooldown; //Cooldown period of the projectile attack
+    public GameObject target; //Reference to the target to be attacked
+    public GameObject projectile; //Reference to the projectile prefab which the furnaceFly will shoot
+    public float projectileSpeed; //Speed of the projectile 
+    public float aggroRange; //Clear name
 
-    private bool movingUp;
-    private GameObject projectileClone;
-    private float localAttackCooldown = 0;
-    private Transform projectileSpawnLocation;
-    // Start is called before the first frame update
+    private bool movingUp; //State used for vertical motion
+    private GameObject projectileClone; //Reference to work with each spawned projectile
+    private float localAttackCooldown = 0; // Local variable to use for cooldown of the attack
+    private Transform projectileSpawnLocation; //Adjustable child object to determine where the projectile is spawned from
+    private float upperBound; //Adjustable child object to determine how far up the furnaceFly goes
+    private float lowerBound; //Adjustable child object to determine how far down the furnaceFly goes
+    private Rigidbody2D projectileRGB; //Reference to be used to store the rigidbody component of the projectile when it is spawned.
+
     void Start()
     {
         if(target == null)
         target = GameObject.FindGameObjectWithTag("Player");
         projectileSpawnLocation = gameObject.transform.GetChild(0).transform;
+        upperBound = transform.Find("UpperBound").position.y;
+        lowerBound = transform.Find("LowerBound").position.y;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+        
+
+        //Flipping furnacefly according to where the target is
+        if (target.transform.position.x < transform.position.x)
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        else
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+
+        //Using update frames to run the cooldown of the projectile attack
         if (localAttackCooldown > 0)
             localAttackCooldown -= Time.deltaTime;
-        else
+        else   //Launch projectile
         {
             projectileClone = GameObject.Instantiate(projectile, projectileSpawnLocation.position, transform.rotation);
             localAttackCooldown = attackCooldown;
             Debug.Log("This is the rigidbody velocity" + projectileClone.GetComponent<Rigidbody2D>().velocity);
-            projectileClone.GetComponent<Rigidbody2D>().velocity = (target.transform.position - transform.position).normalized * projectileSpeed;
-            
+            projectileRGB = projectileClone.GetComponent<Rigidbody2D>();
+            projectileRGB.velocity = (target.transform.position - transform.position).normalized * projectileSpeed;
+            float angle = Mathf.Atan2(projectileRGB.velocity.y, projectileRGB.velocity.x) * Mathf.Rad2Deg;
+            projectileClone.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
             Debug.Log("Cooldown Reset");
         }
 
+        //Simple state system to switch between upward and downward motion
         if (movingUp)
         {
-            if (transform.position.y <= yMax)
+            if (transform.position.y <= upperBound)
                 transform.position = new Vector3(transform.position.x, transform.position.y + speed * Time.deltaTime);
             else
                 movingUp = !movingUp;
@@ -50,7 +66,7 @@ public class FurnaceFly : MonoBehaviour
         }
         else
         {
-            if (transform.position.y >= yMin)
+            if (transform.position.y >= lowerBound)
                 transform.position = new Vector3(transform.position.x, transform.position.y - speed * Time.deltaTime);
             else
                 movingUp = !movingUp;
